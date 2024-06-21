e44bfeeb8dfecf9947703409a8739c48d11ecf82
Julio Fernandes <julio.fernandes7@gmail.com>
2024-06-21 20:26:13 -0300

    adicionando modulo de LocalStorage com suporte para SwiftData

---
+++ libraries/core/LocalStorage/LocalStorage/LocalStorageRegister.swift
@@ -0,0 +1,8 @@
+import Foundation
+import DependencyInjector
+
+public final class LocalStorageRegister: RegisterDependenciesProtocol {
+    public static func register(container: ContainerRegisterProtocol) {
+        
+    }
+}
