c5926b346f2b98b21a6433dad47bd8bfd84bddeb
Julio Fernandes <julio.fernandes7@gmail.com>
2024-06-30 18:26:35 -0300

    Criando layout em swiftUI

--- libraries/ui/AuthenticationUI/AuthenticationUI/IntroScreen/IntroScreen.swift
+++ libraries/ui/AuthenticationUI/AuthenticationUI/IntroScreen/IntroScreen.swift
@@ -1,15 +1,47 @@
-//
-//  IntroScreen.swift
-//  AuthenticationUI
-//
-//  Created by Julio Fernandes on 30/06/24.
-//
-
 import SwiftUI
+import DesignSystem
 
-struct IntroScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct IntroScreen: View {
+    public init() {}
+    
+    public var body: some View {
+        VStack {
+            Spacer()
+            Text("Trainer")
+                .font(.largeTitle)
+                .foregroundColor(.colorFrom(dsColor: .neutralSoftest))
+                .padding(.bottom)
+            Spacer()
+            VStack {
+                Button(action: {
+                    // Ação para criar conta
+                }) {
+                    Text("Entrar")
+                        .textCase(.uppercase)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.colorFrom(dsColor: .neutralStronger))
+                        .foregroundColor(.colorFrom(dsColor: .primary))
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                
+                Button(action: {
+                    // Ação para login
+                }) {
+                    Text("Criar conta")
+                        .textCase(.uppercase)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.colorFrom(dsColor: .primary))
+                        .foregroundColor(.colorFrom(dsColor: .neutralStronger))
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom, 30)
+        }
+        .background(Color.colorFrom(dsColor: .neutralStronger).edgesIgnoringSafeArea(.all))
     }
 }
 
